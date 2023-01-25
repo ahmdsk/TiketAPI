5b73616b53c653bcabc908a694d9dbe8a84aaeff
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-01-25 14:13:24 +0700

    [Models] Update Model UserEvent & Ticket

--- app/Models/Ticket.ts
+++ app/Models/Ticket.ts
@@ -1,9 +1,35 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import Event from './Event'
 
 export default class Ticket extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public code: string
+
+  @column()
+  public amount: number
+
+  @column()
+  public id_used: boolean
+
+  @column()
+  public userId: number
+
+  @column()
+  public eventId: number
+
+  @column()
+  public used_date: string
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+
+  @belongsTo(() => Event)
+  public event: BelongsTo<typeof Event>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
