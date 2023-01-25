43b64f2cc9b9e091b0f07dcda0b954b1e1fdb6b0
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-01-25 12:21:47 +0700

    [Model] Add Model Event & Ticket

---
+++ app/Models/Event.ts
@@ -0,0 +1,37 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+
+import Ticket from './Ticket'
+import User from './User'
+
+export default class Event extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public description: string
+
+  @column()
+  public date: string
+
+  @column()
+  public ticket_price: number
+
+  @column()
+  public userId: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @hasMany(() => Ticket)
+  public tickets: HasMany<typeof Ticket>
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+}
