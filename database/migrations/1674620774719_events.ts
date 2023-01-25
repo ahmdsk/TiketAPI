be920c85b33b58b8aaa1ff7b330042703d588378
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-01-25 12:13:43 +0700

    [Migration] Add All Table Migration

---
+++ database/migrations/1674620774719_events.ts
@@ -0,0 +1,18 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Events extends BaseSchema {
+  protected tableName = 'events'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).nullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1674620775404_tickets.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Tickets extends BaseSchema {
+  protected tableName = 'tickets'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('code')
+      table.double('amount')
+
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('event_id').unsigned().references('id').inTable('events').onDelete('CASCADE')
+      table.boolean('is_used').defaultTo(false)
+      table.dateTime('used_date').nullable()
+      
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).nullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
